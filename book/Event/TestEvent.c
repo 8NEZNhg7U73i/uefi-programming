756089460fae4285b783ae899ee3214d7cf48770
8NEZNhg7U73i <68855943+8NEZNhg7U73i@users.noreply.github.com>
2024-06-03 17:38:18 +0000

    Update TestEvent.c

--- book/Event/TestEvent.c
+++ book/Event/TestEvent.c
@@ -157,7 +157,7 @@ EFI_STATUS TestEventSingal()
     EFI_EVENT TimeEvent;
     KeyEvent = gST->ConIn->WaitForKey;
     
-    Unsigned short TimeNotifyContext = L"Hello! Time Out!";
+    unsigned short TimeNotifyContext = L"Hello! Time Out!";
     Print(L"Test EVT_TIMER | EVT_NOTIFY_SIGNAL\n");
 
     // 生成Timer事件，并设置触发函数
