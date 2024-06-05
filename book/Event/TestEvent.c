33606dc5e89e1e6be78fe40b06afa7bc4e1dce52
8NEZNhg7U73i <68855943+8NEZNhg7U73i@users.noreply.github.com>
2024-06-05 14:55:23 +0000

    Update TestEvent.c

--- book/Event/TestEvent.c
+++ book/Event/TestEvent.c
@@ -20,6 +20,7 @@ VOID testMouseSimple(IN EFI_EVENT Event, IN VOID *Context)
     EFI_SIMPLE_POINTER_PROTOCOL* mouse = 0;
     EFI_SIMPLE_POINTER_STATE     State;
     EFI_EVENT events[2]; // = {0, gST->ConIn->WaitForKey};
+    EFI_INPUT_KEY     Key;
     //显示光标
     gST->ConOut->EnableCursor (gST->ConOut, TRUE);
     //找出鼠标设备
@@ -37,7 +38,6 @@ VOID testMouseSimple(IN EFI_EVENT Event, IN VOID *Context)
     events[0] = mouse->WaitForInput;
     // 将键盘事件放到等待数组
     events[1] = gST->ConIn->WaitForKey;
-    EFI_INPUT_KEY     Key;
     UINTN index;
     // 等待events中的任一事件发生
     Status = gBS->WaitForEvent(2, events, &index);
