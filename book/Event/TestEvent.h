724c91ba9d8e331ef014eda3092723030d0bfc72
8NEZNhg7U73i <68855943+8NEZNhg7U73i@users.noreply.github.com>
2024-06-05 16:28:22 +0000

    Update TestEvent.h

--- book/Event/TestEvent.h
+++ book/Event/TestEvent.h
@@ -2,3 +2,12 @@ VOID TimeNotify(IN EFI_EVENT Event, IN VOID *Context);
 VOID KeyNotify(IN EFI_EVENT Event, IN VOID *Context);
 
 
+EFI_SIMPLE_TEXT_INPUT_PROTOCOL                      *SimpleInput;
+
+EFI_INPUT_KEY Key;
+EFI_STATUS Status;
+UINTN waitidx;
+CHAR16 *KeyNotifyContext = L"TimeNotify!";
+EFI_EVENT KeyEvent;
+KeyEvent = gST->ConIn->WaitForKey;
+
